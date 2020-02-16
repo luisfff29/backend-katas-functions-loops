0817bea182dc3e4a04cc69eb1cded5888397aa2d
luisfff29 <luisfff29@gmail.com>
2020-02-16 02:48:50 -0600

    power() done

--- main.py
+++ main.py
@@ -31,10 +31,8 @@ def power(x, n):
     """Raise x to power n, where n >= 0"""
     if n < 0:
         return "n can't be a negative number"
-    elif n == 0:
-        return 1
-    elif x == 0:
-        return 0
+    elif n == 0 or x == 0:
+        return 0 if x == 0 else 1
     elif n == 1:
         return x
     else:
